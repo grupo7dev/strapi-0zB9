1644a948671a6a3c9f957cbe4690ad3e1afec645
grupo7dev <145135641+grupo7dev@users.noreply.github.com>
2023-09-20 12:48:59 -0300

    Update plugins.js nodemailer added!!

--- config/plugins.js
+++ config/plugins.js
@@ -1,21 +1,60 @@
 module.exports = ({ env }) => ({
-    'users-permissions': {
-      config: {
-      jwtSecret: env('JWT_SECRET'),
+  "file-system": {
+    enabled: true,
+  },
+  upload: {
+    config: {
+      provider: "cloudinary",
+      providerOptions: {
+        cloud_name: env("CLOUDINARY_NAME"),
+        api_key: env("CLOUDINARY_KEY"),
+        api_secret: env("CLOUDINARY_SECRET"),
+      },
+      actionOptions: {
+        upload: {},
+        uploadStream: {},
+        delete: {},
       },
     },
-    upload: {
-      config: {
-        provider: 'cloudinary',
-        providerOptions: {
-          cloud_name: env('CLOUDINARY_NAME'),
-          api_key: env('CLOUDINARY_KEY'),
-          api_secret: env('CLOUDINARY_SECRET'),
+  },
+  email: {
+    config: {
+      provider: "nodemailer",
+      providerOptions: {
+        host: env("SMTP_HOST"),
+        port: env("SMTP_PORT"),
+        auth: {
+          user: env("SMTP_USERNAME"),
+          pass: env("SMTP_PASSWORD"),
         },
-        actionOptions: {
-          upload: {},
-          delete: {},
-        },
+        // ... any custom nodemailer options
+      },
+      settings: {
+        defaultFrom: env("SMTP_USERNAME"),
       },
     },
-  });
+  },
+});
+
+
+// module.exports = ({ env }) => ({
+//     'users-permissions': {
+//       config: {
+//       jwtSecret: env('JWT_SECRET'),
+//       },
+//     },
+//     upload: {
+//       config: {
+//         provider: 'cloudinary',
+//         providerOptions: {
+//           cloud_name: env('CLOUDINARY_NAME'),
+//           api_key: env('CLOUDINARY_KEY'),
+//           api_secret: env('CLOUDINARY_SECRET'),
+//         },
+//         actionOptions: {
+//           upload: {},
+//           delete: {},
+//         },
+//       },
+//     },
+//   });
